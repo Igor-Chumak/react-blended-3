ec385a3be6f91af2619f69b39d44974e454dcc33
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-08-20 15:40:54 +0300

    home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { Container, CountryList, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/country-service';
+
+export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    getCountries()
+      .then(data => {
+        setCountries(data);
+      })
+      .catch()
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <Section>
+      <Container>
+        <CountryList countries={countries} />
+        {isLoading && <Loader />}
+      </Container>
+    </Section>
+  );
+};
